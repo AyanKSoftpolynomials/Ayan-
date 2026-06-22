0f6d0c4e535b77f9fe4e3ff58d114e02aaaedcb5
AyanKSoftpolynomials <ayank@softpolynomials.com>
2026-06-22 12:55:13 +0530

    change

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+    
+    ],
+    credentials: true,
+  })
+);
